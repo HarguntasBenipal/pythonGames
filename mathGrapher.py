c4b744f6bcbff0b41009743f1b6012681c093724
HarguntasBenipal <115516035+HarguntasBenipal@users.noreply.github.com>
2023-06-21 13:22:31 -0400

    Update mathGrapher.py

--- mathGrapher.py
+++ mathGrapher.py
@@ -12,15 +12,19 @@ a = int(input("a: "))
 b = int(input("b: "))
 c = int(input("c: "))
 
+#Use quadratic formula to determine x-ints
 x1 = (-b + math.sqrt(b**2-4*a*c))/2*a
 x2 = (-b - math.sqrt(b**2-4*a*c))/2*a
 print(str(x1) + " and " + str(x2))
 
  #a = arr.array([0],[c])
 z=0
+
+#Create a x & y value array to make table of values
 ax = arr.array("i", [0])
 ay = arr.array("i", [c])
 
+#go up to x=5 and get y value in order to graph
 for i in range (5):
   y = (a*i)**2+b*i+c
   ax.append(i)
